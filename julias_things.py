b084ebd7edd40deab6485d0463424c61cece996f
Julia <jgaubatz@mit.edu>
2023-01-09 16:45:36 -0500

    Edited pipe losses

--- julias_things.py
+++ julias_things.py
@@ -4,7 +4,6 @@ import colebrook
 transport_efficiency_MJ_per_seat_km = 0.008 # kg/seat-km
 
 ### My calculations
-
 ## Constants
 hydrogen_density = 70 #kg/m^3
 hydrogen_dynamic_viscosity = 1.4E-5 #Pa*s
@@ -13,16 +12,19 @@ pipe_roughness = 0.045 #mm
 # Pipe Design
 pipe_length = 10 #m
 pipe_diameter = 0.5 #m
-mass_flow_rate = 50 #kg/s
-#Intermediate values
-pipe_area = 3.1415*(pipe_diameter/2)**2 #m^2
-volume_flow_rate = mass_flow_rate/hydrogen_density #m^3/s
-speed = volume_flow_rate/pipe_area #m/s
-#Friction Factor Calculation
-Re = speed*pipe_diameter*hydrogen_density/hydrogen_dynamic_viscosity
-relative_roughness = pipe_roughness/1000/pipe_diameter
-darcy_friction_factor = colebrook.sjFriction( Re, relative_roughness)
-#Solution
-dP = pipe_length*darcy_friction_factor*hydrogen_density*(1/2)/pipe_diameter*speed**2 #Pa
-dH = dP/9.81/hydrogen_density #m, head loss
-print(dP, darcy_friction_factor)
+fueling_flow_rate = 50 #kg/s
+def pipe_losses(length, diameter, mass_flow_rate):
+    #Intermediate values
+    pipe_area = 3.1415*(diameter/2)**2 #m^2
+    volume_flow_rate = mass_flow_rate/hydrogen_density #m^3/s
+    speed = volume_flow_rate/pipe_area #m/s
+    #Friction Factor Calculation
+    Re = speed*diameter*hydrogen_density/hydrogen_dynamic_viscosity
+    relative_roughness = pipe_roughness/1000/pipe_diameter
+    darcy_friction_factor = colebrook.bntFriction( Re, relative_roughness)
+    #Solution
+    dP = length*darcy_friction_factor*hydrogen_density*(1/2)/diameter*speed**2 #Pa
+    dH = dP/9.81/hydrogen_density #m, head loss
+    print(dP, darcy_friction_factor)
+
+pipe_losses(pipe_length, pipe_diameter, fueling_flow_rate)
