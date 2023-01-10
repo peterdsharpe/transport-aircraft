043ffe8980e4b9ea3fff0bff1d72a4c269b0e84b
Julia <jgaubatz@mit.edu>
2023-01-10 13:45:27 -0500

    Calculated energy for pumping as a percent of total required energy

--- julias_things.py
+++ julias_things.py
@@ -20,11 +20,23 @@ def pipe_losses(length, diameter, mass_flow_rate):
     #Solution
     dP = length*darcy_friction_factor*hydrogen_density*(1/2)/diameter*speed**2 #Pa
     dH = dP/9.81/hydrogen_density #m, head loss
-    print(dP, darcy_friction_factor)
+    print("Pressure Drop: ", dP, " | Head Loss: ", dH, " | Friction Factor: ", darcy_friction_factor)
+    return dP, dH
 
 ## Pipe flow losses
 # Pipe Design
 pipe_length = 10 #m
 pipe_diameter = 0.5 #m
+refueling_time = 9.81 #min
 fueling_flow_rate = 50 #kg/s
-pipe_losses(pipe_length, pipe_diameter, fueling_flow_rate)
+[dP, dH] = pipe_losses(pipe_length, pipe_diameter, fueling_flow_rate)
+pump_power_refueling = dH*fueling_flow_rate*9.81 #W
+[dP, dH] = pipe_losses(19000, 0.4, fueling_flow_rate)
+pump_power_pipeline = dH*fueling_flow_rate*9.81 #W
+pump_power_hp = pump_power_pipeline/746 #hp
+print("Pump Power [hp]: ", pump_power_hp)
+
+energy_demand_ORD = 42200000 #kWh/day
+energy_demand_pump = pump_power_pipeline*24/1000 + pump_power_refueling*refueling_time/60/1000 #kWh/day
+pump_energy_percent = energy_demand_pump/(energy_demand_ORD+energy_demand_pump)*100
+print("Pump Energy Percent of Total Energy Required: ", pump_energy_percent)
